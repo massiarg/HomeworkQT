49c2d74481f549a58115c727ec47314e38bfe804
massiarg <hyuugamassi@gmail.com>
2017-11-27 01:44:42 +0100

    making declaration on the header file mainwindow for the method                                                                                                                                  s to be used loadwebpage, myreplyfished, savetofile, setstrd, getstrd, networkac                                                                                                                                  cessmanager and strd

--- HomeWorkQT/mainwindow.h
+++ HomeWorkQT/mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QNetworkAccessManager>
 namespace Ui {
 class MainWindow;
 }
@@ -11,12 +11,26 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+    QNetworkAccessManager* manager;
+
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void setstrd(QString in);
+    void saveToFile(QList<QPair<QString, QString>> in);
+
+private slots:
+    void on_Btn1_clicked();
+    void on_Btn2_clicked();
+
+public slots:
+    void loadWebPage();
+    void myReplyFinished(QNetworkReply* reply);
+    QString getstrd();
 
 private:
     Ui::MainWindow *ui;
+    QString strd;
 };
 
 #endif // MAINWINDOW_H
