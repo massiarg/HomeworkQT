e0ae773ff2fb64202502bb48f0a9416cfe9126ab
massiarg <hyuugamassi@gmail.com>
2017-11-28 00:54:43 +0100

    making declaration of values setValues getValues and makeplot

--- HomeWorkQT/plotwindow.h
+++ HomeWorkQT/plotwindow.h
@@ -14,9 +14,16 @@ class Plotwindow : public QDialog
 public:
     explicit Plotwindow(QWidget *parent = 0);
     ~Plotwindow();
+    void setValues(QList<QPair<QString, QString>> in);
+    QList<QPair<QString, QString>> getValues();
+
+public slots:
+    void makeplot(QList<QPair<QString, QString>> in);
+
 
 private:
     Ui::Plotwindow *ui;
+    QList<QPair<QString, QString>> values;
 };
 
 #endif // PLOTWINDOW_H
