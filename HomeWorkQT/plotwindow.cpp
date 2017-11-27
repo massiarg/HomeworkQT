a22c3d59396a7fc39687884f846e29ce75572bc0
massiarg <hyuugamassi@gmail.com>
2017-11-28 00:55:52 +0100

    making definition of values setValues getValues and makeplot

--- HomeWorkQT/plotwindow.cpp
+++ HomeWorkQT/plotwindow.cpp
@@ -12,3 +12,33 @@ Plotwindow::~Plotwindow()
 {
     delete ui;
 }
+
+void Plotwindow::setValues(QList<QPair<QString, QString> > in){
+    Plotwindow::values = in;
+}
+
+QList<QPaire<QString, QString>> Plotwindow::getValues(){
+    return Plotwindow::values;
+}
+
+void Plotwindow::makeplot(QList<QPair<QString, QString> > in){
+    QVector<double> x(in.size()), y(in.size());
+    for (int i = 0; i<in.size(); i++){
+        x[i] = (double)i;
+        QPair<QString, QString> temp = in[i];
+        double tmpd = temp.second.toDouble();
+        y[i] = tmpd;
+    }
+    ui->customPlot->addGraph();
+    ui->customPlot->graph(0)->setData(x, y);
+    ui->customPlot->graph(0)->setPen(QPen(Qt::blue));
+    ui->customPlot->graph(0)->setBrush(QBrush(QColor(0, 0, 255, 20)));
+
+    // give the axes some labels:
+    ui->customPlot->xAxis->setLabel("x");
+    ui->customPlot->yAxis->setLabel("y");
+    // set axes ranges, so we see all data:
+    ui->customPlot->xAxis->setRange(0, in.size());
+    ui->customPlot->yAxis->setRange(0, 87);
+    ui->customPlot->replot();
+}
