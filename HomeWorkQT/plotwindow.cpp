caa1093312ffdb4fdcd9c29edf6bd3592ef0abbb
massiarg <hyuugamassi@gmail.com>
2017-11-28 02:37:52 +0100

    definition of makeplotvolume because the values are too big we created a method

--- HomeWorkQT/plotwindow.cpp
+++ HomeWorkQT/plotwindow.cpp
@@ -6,6 +6,7 @@ Plotwindow::Plotwindow(QWidget *parent) :
     ui(new Ui::Plotwindow)
 {
     ui->setupUi(this);
+
 }
 
 Plotwindow::~Plotwindow()
@@ -19,6 +20,28 @@ void Plotwindow::setValues(QList<QPair<QString, QString> > in){
 
 QList<QPair<QString, QString>> Plotwindow::getValues(){
     return Plotwindow::values;
+}
+
+void Plotwindow::makeplotVolume(QList<QPair<QString, QString> > in){
+    QVector<double> x(in.size()), y(in.size());
+    for (int i = 0; i<in.size(); i++){
+        x[i] = (double)i;
+        QPair<QString, QString> temp = in[i];
+        double tmpd = temp.second.toDouble();
+        y[i] = tmpd/1000000.0;
+    }
+    ui->customPlot->addGraph();
+    ui->customPlot->graph(0)->setData(x, y);
+    ui->customPlot->graph(0)->setPen(QPen(Qt::blue));
+    ui->customPlot->graph(0)->setBrush(QBrush(QColor(0, 0, 255, 20)));
+
+    // give the axes some labels:
+    ui->customPlot->xAxis->setLabel("x");
+    ui->customPlot->yAxis->setLabel("y * 1million");
+    // set axes ranges, so we see all data:
+    ui->customPlot->xAxis->setRange(0, in.size());
+    ui->customPlot->yAxis->setRange(0, 87);
+    ui->customPlot->replot();
 }
 
 void Plotwindow::makeplot(QList<QPair<QString, QString> > in){
